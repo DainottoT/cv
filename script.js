fbe98854c028e80090b9072c334b73f93dad6144
DainottoT <dainottotamara@gmail.com>
2023-01-15 19:25:18 -0300

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("iniciar").addEventListener("click", displayCv);
+
+function displayCv() {
+    document.getElementById("Curriculum").style.display = "block";
+    document.getElementById("iniciar").style.display = "none";
+}
